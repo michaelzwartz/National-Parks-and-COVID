468aabfe87871b67df96e5f078cc8302ebe6e7a1
mzwartz <michaelzwartz@lewisu.edu>
2023-06-10 14:29:40 -0500

    updated

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -6,13 +7,13 @@ import matplotlib.pyplot as plt
 tot = pd.read_csv('NPS Total Visitors.csv')
 
 #create a new column in datafram for %change from the previous year
-i = tot.shape[0]-2
+i = tot.shape[0]-1
 tot['Change'] = 0
-while i > -1: 
-    tot['Change'].iloc[i] = (tot['Visitor Count'].iloc[i+1] - 
-                              tot['Visitor Count'][i])/tot['Visitor Count'].iloc[i+1]*100
+while i > 0: 
+   tot['Change'].iloc[i] = (tot['Visitor Count'].iloc[i] - 
+                              tot['Visitor Count'][i-1])/tot['Visitor Count'].iloc[i-1]*100
+   i = i-1
 
-    i = i-1
 
 #load the by park (2000 - 2022)
 by_park = pd.read_csv('Annual Visitation By Park.csv')
@@ -23,3 +24,10 @@ by_park = by_park[['ParkName', 'UnitCode', 'ParkType', 'Region', 'State', 'Year'
 #change visit from string to integers 
 by_park['RecreationVisits'] = by_park['RecreationVisits'].replace(',','', regex=True)
 by_park['RecreationVisits'] = by_park['RecreationVisits'].apply(pd.to_numeric)
+
+#output to csv
+cwd = os.getcwd()
+path1 = cwd + "\Clean Data\AnnualVisits_1900-2022.csv"
+path2 = cwd + "\Clean Data\MonthlyVisits_2000-2022.csv"
+tot.to_csv(path1, index = False)
+by_park.to_csv(path2, index=False)
